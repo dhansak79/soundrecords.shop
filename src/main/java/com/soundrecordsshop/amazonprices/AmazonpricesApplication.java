67b36359d6dad7ebd2bd5e8c800fa5fe52e0eb2f
dhansak79 <daniel.webb@me.com>
2024-02-14 18:06:22 +0000

    add bnase project

---
+++ src/main/java/com/soundrecordsshop/amazonprices/AmazonpricesApplication.java
@@ -0,0 +1,13 @@
+package com.soundrecordsshop.amazonprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonpricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonpricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soundrecordsshop/amazonprices/AmazonpricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundrecordsshop.amazonprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonpricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
